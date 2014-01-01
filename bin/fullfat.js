f4ce1f12f7f01fba35b99531126aad29c7da040f
isaacs <i@izs.me>
2014-01-01 15:33:50 -0800

    bin script

---
+++ bin/fullfat.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+var FF = require('../fullfat.js')
+var dashdash = require('dashdash')
+
+var parser = dashdash.createParser({
+  options: [
+    { names: [ 'fat', 'f' ],
+      type: 'string',
+      help: 'The database that has attachments. Required.'
+      helpArg: 'URL' },
+    { names: [ 'skim', 's' ],
+      type: 'string',
+      help: 'The database without attachments.  Required.',
+      helpArg: 'URL' },
+    { names: [ 'tmp', 't' ],
+      type: 'string',
+      default: null,
+      help: 'Path to folder where temp files are stored.',
+      helpArg: 'PATH' },
+    { names: [ 'user-agent', 'ua', 'u' ],
+      type: 'string',
+      default: null,
+      help: 'User-Agent header that npm FullFat sends.',
+      helpArg: 'AGENT' },
+    { names: [ 'seq-file', 'q' ],
+      type: 'string',
+      help: 'File to store the sequence id in.',
+      helpArg: 'FILE' },
+    { names: [ 'inactivity-ms', 'i' ],
+      type: 'number',
+      help: 'Max ms to wait before assuming disconnection. Default=3600000',
+      helpArg: 'MS' },
+    { names: [ 'help', 'h' ],
+      type: 'bool',
+      help: 'Display this help' }
+  ]
+})
+
+opts = parser.parse(process.argv, process.env)
+
+if (opts.help)
+  return usage(true)
+
+function usage(ok) {
+  console.log(parser.help())
+  process.exit(ok ? 0 : 1)
+}
+
+try {
+  var ff = new FF(opts)
+} catch (er) {
+  console.error(er.message)
+  usage(false)
+}
+
+ff.on('start', function() {
+  console.log('START %s pid=%d', ff.ua, process.pid)
+})
+
+ff.on('change', function(change) {
+  console.log('Received Change', change.id)
+})
+
+ff.on('putDesign', function(doc, result) {
+  console.log('PUT %s %j', doc._id, result)
+})
+
+ff.on('put', function(doc, result) {
+  console.log('PUT %s %j', doc._id, result)
+})
+
+ff.on('delete', function(data) {
+  console.log('DELETE %s %j', data.name, data)
+})
+
+ff.on('error', function(er) {
+  console.log('ERROR', er)
+  throw er
+})
